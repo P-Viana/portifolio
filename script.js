a6b20472e71011e763cb0cbc6f1296e19225a857
P-Viana <viana.pedro.barbosa@gmail.com>
2024-10-30 10:57:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,34 @@
-var testa = 0;
-var darktheme = document.body;
-
-function darkMode()
-{
-  //Tentar salvar os valores do modo escuro/ claro.
-  localStorage.setItem("active-mode", testa);
-  document.getElementsByClassName("buttontema").innerHTML = localStorage.getItem("active-mode");
-  
-   if(testa == 0)
-   {
-    //Ativar o DarkMode
-    darktheme.classList.toggle("dark-mode");
-    document.getElementById('emailicon').style.fill = "white";
-    document.getElementById('githubicon').style.fill = "white";
-    //Mudar o valor para depois mudar o tema de novo
-    testa = 1;
-   }
-   else
-   {
-    //Ativar o DarkMode
-    darktheme.classList.toggle("dark-mode");
-    document.getElementById('emailicon').style.fill = "black";
-    document.getElementById('githubicon').style.fill = "black";
-   //Mudar o valor para depois mudar o tema de novo
-    testa = 0;
-   }
+function darkmode(){
+  const darkelement = document.body
+  if (check.checked) {
+      darkelement.classList.add("dark-mode")
+      localStorage.setItem("darktheme",  "true")
+  }
+  else{
+      darkelement.classList.remove("dark-mode")
+      localStorage.setItem("darktheme",  "false")
+  }
 }
 
+addEventListener("DOMContentLoaded", ()=>{
+  const darkMOdeStatus = localStorage.getItem("darktheme");
+  if (darkMOdeStatus === "true") {
+      check.checked = true;
+  }
+  else{
+      
+     check.checked = false
+  }
+  darkmode();
+})
 
+check.addEventListener("change", darkmode);
 
 
 //Código para o Typing Effect
 var i = 0;
 var txt = 'Bem-vindo ao meu portfólio!'; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById("escrevertexto").innerHTML += txt.charAt(i);
@@ -43,5 +36,4 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
-
 typeWriter()
